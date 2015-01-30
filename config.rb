a3f77ff6da2accd588e4a383dc9ffd8085e371cd
Jayant Gandhi <jayant.gandhi@gmail.com>
2015-01-30 15:09:22 +0100

    up and running with scss files

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/public"
-css_dir = "stylesheets"
+css_dir = "/public/stylesheets"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+images_dir = "/public/images"
+javascripts_dir = "/public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed
